14cc41d36ff376f732a676eafb84152e233f693a
Gde Skidka Dev <dev@gde-skidka.ru>
2026-08-24 12:37:58 +0300

    Remove leaflet watermark

--- frontend/src/app/components/MapBackground.tsx
+++ frontend/src/app/components/MapBackground.tsx
@@ -9,11 +9,11 @@ export default function MapBackground() {
       center={[55.751574, 37.573856]} // Москва по умолчанию
       zoom={13} 
       zoomControl={false}
+      attributionControl={false} // Убираем вотермарку Leaflet
       style={{ width: "100%", height: "100%" }}
     >
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
       />
     </MapContainer>
   );
